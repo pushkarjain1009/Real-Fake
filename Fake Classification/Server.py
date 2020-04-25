e966f6897d9477e8006b57a70384cadd2c557b2a
pushkarjain1009 <pushkarjain1009@gmail.com>
2020-04-26 03:49:14 +0530

    For Web App

---
+++ Fake Classification/Server.py
@@ -0,0 +1,30 @@
+import cv2
+#from tensorflow.keras.models import load_model
+from flask import Flask, render_template, request, url_for
+
+app = Flask(__name__)
+
+#model = load_model("model/model.h5")
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/result', methods=["GET"])
+def result():
+    img = request.files["myimage"]
+    img.save(secure_filename(img.filename))
+    
+    Img = cv2.imread(img.filename)
+    Img = cv2.resize(Img, (128,128))
+
+    #res = model.predict(Img)
+    res = 0
+    if res == 0:
+        return render_template('result.html', result = "REAL")
+    else:
+        return render_template('result.html', result = "FAKE")
+
+if __name__ == '__main__':
+    app.run(debug=True)
